cf1556e56ae7db79d1d8cd3b6794ddcc66768923
Sandro Müller Scheuermann <sandroryze@gmail.com>
2023-09-02 21:38:10 -0300

    removendo codigo comentado

--- lua/config/remappings.lua
+++ lua/config/remappings.lua
@@ -10,8 +10,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("n", "Q", "<nop>") -- reza a lenda que é zoado apertar Q...
 
---vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
 vim.keymap.set("n", "<C-q>", vim.cmd.tabNext)
 vim.keymap.set("n", "<C-e>", vim.cmd.tabnext)
 vim.keymap.set("n", "<C-w>", vim.cmd.tabclose)
