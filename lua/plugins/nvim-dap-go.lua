11b53cfbb067bfe356413d54974d5304b6e11e52
Sandro Müller Scheuermann <sandroryze@gmail.com>
2023-09-03 22:10:22 -0300

    ajustando toggle breakpoint

--- lua/plugins/nvim-dap-go.lua
+++ lua/plugins/nvim-dap-go.lua
@@ -4,8 +4,8 @@ local P = {
     dependencies = "mfussenegger/nvim-dap",
     config = function() require("dap-go").setup() end,
     keys = {
-        { '<leader>db', function() vim.cmd("DapToggleBreakpoint <CR>") end },
-        { '<leader>di', function()
+        { "<leader>db", ":DapToggleBreakpoint<CR>", desc = "Toggle breakpoint" },
+        { '<leader>dio', function()
             local widgets = require('dap.ui.widgets');
             local sidebar = widgets.sidebar(widgets.scopes);
             sidebar.open()
