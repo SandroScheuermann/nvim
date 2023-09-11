e56f20e1efc11c8c4359bad1f9a588b1945d2767
Sandro Müller Scheuermann <sandroryze@gmail.com>
2023-09-11 00:19:03 -0300

    ajustando dap do go

--- lua/plugins/nvim-dap-go.lua
+++ lua/plugins/nvim-dap-go.lua
@@ -1,15 +1,19 @@
 local P = {
     "leoluz/nvim-dap-go",
     ft = "go",
-    dependencies = "mfussenegger/nvim-dap",
+    dependencies = { "mfussenegger/nvim-dap", "rcarriga/nvim-dap-ui" },
     config = function() require("dap-go").setup() end,
     keys = {
-        { "<leader>db", ":DapToggleBreakpoint<CR>", desc = "Toggle breakpoint" },
+        { "<F9>", ":DapToggleBreakpoint<CR>", desc = "Toggle breakpoint" },
+        { "<F5>",         ":DapContinue<CR>",         desc = "Start/Continue debugging" },
+        { "<F10>", ":DapStepOver<CR>", desc = "Step over" },
+        { "<F11>", ":DapStepInto<CR>", desc = "Step into" },
+        { "<F12>", ":DapStepOut<CR>", desc = "Step out" },
         { '<leader>dio', function()
             local widgets = require('dap.ui.widgets');
             local sidebar = widgets.sidebar(widgets.scopes);
             sidebar.open()
-        end },
+        end }
     }
 }
 return P
