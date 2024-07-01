a04bd7bbb9974c763c8396b000b20846b6c43fd8
Sandro <sandroryze@gmail.com>
2024-07-01 20:02:06 -0300

    Configurandinho

--- lua/plugins/nvim-dap-go.lua
+++ lua/plugins/nvim-dap-go.lua
@@ -4,11 +4,11 @@ local P = {
     dependencies = { "mfussenegger/nvim-dap", "rcarriga/nvim-dap-ui" },
     config = function() require("dap-go").setup() end,
     keys = {
-        { "<F9>", ":DapToggleBreakpoint<CR>", desc = "Toggle breakpoint" },
-        { "<F5>",         ":DapContinue<CR>",         desc = "Start/Continue debugging" },
-        { "<F10>", ":DapStepOver<CR>", desc = "Step over" },
-        { "<F11>", ":DapStepInto<CR>", desc = "Step into" },
-        { "<F12>", ":DapStepOut<CR>", desc = "Step out" },
+        { "<F9>",  ":DapToggleBreakpoint<CR>", desc = "Toggle breakpoint" },
+        { "<F5>",  ":DapContinue<CR>",         desc = "Start/Continue debugging" },
+        { "<F10>", ":DapStepOver<CR>",         desc = "Step over" },
+        { "<F11>", ":DapStepInto<CR>",         desc = "Step into" },
+        { "<F12>", ":DapStepOut<CR>",          desc = "Step out" },
         { '<leader>dio', function()
             local widgets = require('dap.ui.widgets');
             local sidebar = widgets.sidebar(widgets.scopes);
--- lua/plugins/tabby.lua
+++ lua/plugins/tabby.lua
@@ -11,14 +11,14 @@ local tConfig = function()
 require('tabby.tabline').set(function(line)
     return {
         {
-            { ' 🤓 ', hl = theme.head },
+            { ' - ', hl = theme.head },
             line.sep('|', theme.head, theme.fill),
         },
         line.tabs().foreach(function(tab)
             local hl = tab.is_current() and theme.current_tab or theme.tab
             return {
                 line.sep('|', hl, theme.fill),
-                tab.is_current() and '' or '󰆣',
+                tab.is_current() and '  ' or '󰆣  ',
                 tab.number(),
                 tab.name(),
                 tab.close_btn(''),
