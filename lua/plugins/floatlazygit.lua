97b2e19540c84aafaa49f93299a67fcc61a0d53b
Sandro <sandroryze@gmail.com>
2024-07-11 17:00:39 -0300

    tirando plugins que eu nao uso

--- lua/plugins/floatlazygit.lua
+++
@@ -1,9 +0,0 @@
-local P =
-{
-    "kdheepak/lazygit.nvim",
-    dependencies = {"nvim-lua/plenary.nvim",},
-    keys = {
-        {'<leader>gg',":LazyGit<CR>" },
-    }
-} return P
-
--- lua/plugins/transparent.lua
+++
@@ -1,9 +0,0 @@
-local P = {
-    lazy = false,
-    "xiyaowong/transparent.nvim",
-    config = function() require("transparent").setup({}) end,
-   keys = {
-	{"<leader>tb", ":TransparentToggle<CR>", desc = "Transparent Toggle"},
-    }
-
-} return P
