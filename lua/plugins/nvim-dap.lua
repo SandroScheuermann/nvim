4149675e540c8d0956547c4dc0b066ad97f09307
Sandro <sandroryze@gmail.com>
2024-09-10 23:08:38 -0300

    asdasd

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -62,7 +62,7 @@ local P = {
         vim.keymap.set("n", "<F11>", function() Dap.step_into() end)
         vim.keymap.set("n", "<F12>", function() Dap.step_back() end)
         vim.keymap.set("n", "<leader>ev", function() Ui.eval(nil, { enter = true }) end)
-        vim.keymap.set("n", "<leader>rr", function() Ui.toggle() end)
+        vim.keymap.set("n", "<leader>ui", function() Ui.toggle() end)
 
         Dap.listeners.before.attach.dapui_config = function()
             Ui.open()
